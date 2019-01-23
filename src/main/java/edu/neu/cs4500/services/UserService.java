24ccc62c421248c8b8bce7721338332bce8956ad
nihaalrk <nihaalrk@yahoo.com>
2019-01-23 17:36:04 -0500

    User service findUserById method

--- src/main/java/edu/neu/cs4500/services/UserService.java
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.neu.cs4500.models.User;
@@ -21,5 +22,16 @@ public class UserService {
 		return users;
 	}
 
+	@GetMapping("/api/user/{userId}")
+	public User findUserById(
+		@PathVariable("userId") Integer id) {
+		for(User user: users) {
+			if(user.getId().equals(id)) {
+				return user;
+			}
+		}
+		return null;
+	}
+	
 }
 
