f864a346c77aa21ad1664bb19e03a2cba0987507
nihaalrk <nihaalrk@yahoo.com>
2019-01-23 17:26:40 -0500

    User model constructor, User service findAllUsers function

--- src/main/java/edu/neu/cs4500/models/User.java
+++ src/main/java/edu/neu/cs4500/models/User.java
@@ -6,7 +6,15 @@ public class User {
 	private String password;
 	private String firstName;
 	private String lastName;
-
+	
+	public User(Integer id, String username, String password, String firstName, String lastName) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
  	public String getFirstName() {
 		return firstName;
 	}
